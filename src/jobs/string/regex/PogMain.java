0a04423bcd273f61cd9835935d4aa51ac170daa6
jlucena <jlucena@MC-283.ttrans-sp.net>
2017-07-14 17:59:15 -0300

    PogMain

--- src/jobs/string/regex/PogMain.java
+++ src/jobs/string/regex/PogMain.java
@@ -1,23 +1,35 @@
 package jobs.string.regex;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
 public class PogMain {
+	
+	public static String regexV = "((BAR\\.)(.+?))(.Value(=\\\\d)?)";
 
 	public static void main(String[] args) {
-		String s = "BAR.C01.Digitais.CmdLigaLoc.Value or BAR.C01.Digitais.CmdDesLoc.Value or (BAR.C01.Digitais.MolaCarr.Value=0) or (BAR.C01.Digitais.SupBob1.Value=0) or (BAR.C01.Digitais.SupBob2.Value=0) or BAR.C01.Digitais.ResetBlqBot.Value";
-		String regex ="((BAR)(.+?))(.Value(=\\d)?)";
 		
-		System.out.println(s.replaceAll(regex, "IIF($1.Quality < 192, 0, $0)"));
-		System.out.println(s);
+		String fileName = "C://Dados//BAR.csv";
+
+		try (Stream<String> stream = Files.lines(Paths.get(fileName),Charset.defaultCharset())) {
+			stream.forEach(PogMain::lal);
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		
 	}
 	
+	public static void lal(String a) {
+		System.out.println(a.replaceAll(regexV, "IIF($1.Quality < 192, 0, $0)"));
+	}
 	
-	
-
 	public static List<String> getMatches(String regex, String data, int group) {
 		List<String> s = new ArrayList<>();
 
